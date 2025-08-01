42dfe37880668c6dd38357b0489351cd9ca69eec
abel <abelassefa1292@gmail.com>
2025-08-01 14:02:56 +0300

    testitng telegram notification

--- Backend/server.js
+++ Backend/server.js
@@ -58,6 +58,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port ${PORT}`)
-    console.log(`testing for notification `)
+    console.log(`testing for notification trail 1 `)
 });
 
